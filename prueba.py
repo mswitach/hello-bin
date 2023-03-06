58c72c5acfdfa4b2b9d8b73fb24e9696e770b46b
mswitach <mswitach@hotmail.com>
2023-03-05 22:47:46 -0300

    Update prueba.py
    todo nuevo

--- prueba.py
+++ prueba.py
@@ -1,21 +1,18 @@
-#!/usr/bin/env python
-from binance.um_futures import UMFutures
-import time
+import requests
 import streamlit as st
 
-key1 = 'Huj5XUXd7NXAi7ARFHbPVX3Hq4A0zWCaDoZWhvZno4ZZ9sgj9Mu04HkwS1OcOzVS'
-secret1 = 'O6jfnwHSvksaEGncV0D8G5zlVCl6IrxiDfZXrtsoFELvJRBk2Zq2VsMm1fIfOvfx'
 
-um_futures_client = UMFutures(key=key1, secret=secret1)
+options = [
+    "https://api.binance.com",
+    "https://api1.binance.com",
+    "https://api2.binance.com",
+    "https://api3.binance.com",
+    "https://api.binance.us",
+    "https://data.binance.com",
+]
 
-precio = um_futures_client.ticker_price("BTCUSDT")
-
-aa=float(precio['price'])
-st.write(aa)
-while 1==1:
-    time.sleep(10)
-    precio = um_futures_client.ticker_price("BTCUSDT")
-    bb=float(precio['price'])
-    st.write(bb)
-    st.write("porcentaje = ",round(((bb/aa)-1)*100,2)," %")
-    aa=bb
+base_endpoint = st.selectbox(label="Base endpoint", options=options)
+url = f"{base_endpoint}/api/v3/depth?symbol=BTCBUSD&limit=5"
+st.write(url)
+response = requests.get(url)
+st.write(response.json())
